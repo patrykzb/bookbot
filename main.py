26332cea4332d514f05d0a4c1a2549c13134f42e
Pat <patrykpaszkowski@gmail.com>
2024-11-05 16:30:40 +0100

    letter count

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
+    book = "./books/frankenstein.txt"
+    text = read(book)
+    print(f'--- Begin report of {book} ---')
+    count_words(text)
+    count_letters(text)
+    print("--- End report ---")
+
+
+
+def read(book):
+    with open(book) as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
+def count_words(text):
+        words = text.split()
+        print(f'{len(words)} words found in the document')
+def count_letters(text):
+    small_letters = text.lower()
+    l = list(small_letters)
+    dict_letters = {}
+    for letter in l:
+        if letter in dict_letters:
+            dict_letters[letter] += 1
+        else:
+            dict_letters[letter] = 1
+
+    for letter in dict_letters:
+        if letter.isalpha():
+            print(f"The {letter} character was found {dict_letters[letter]} times")
+    return dict_letters
+
+
+
 main()
